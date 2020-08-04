2a2a3d724bdb2179892ef43de5bcdf0d255aa63e
Nazmia Mouri <edelsteine025@gmail.com>
2020-08-04 20:28:55 +0600

    'HeaderandFooter'

--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
+
 import Menu from './MenuComponents'
 import  Dishdetail from './DishdetailComponent';
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
 import { DISHES } from'../shared/dishes';
 
 class Main extends Component {
@@ -12,7 +14,7 @@ class Main extends Component {
       dishes: DISHES,
       selectedDish:null
     };
-    console.log(this.state.selectedDish)
+    
   }
   onDishSelect(dishId){
     this.setState({selectedDish:dishId});
@@ -22,13 +24,10 @@ class Main extends Component {
 render() {
     return (
       <div>
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
+        <Header/>
         <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
         <Dishdetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+        <Footer/>
       </div>
     );
   }
